4ea58dfd3ff59ad1921a289c84ca3894d6cf922f
Ian Barnes <ian@cryptosense.com>
2021-07-29 19:48:28 +0200

    Reformat with Ocamlformat

--- lib/person.ml
+++ lib/person.ml
@@ -1,12 +1,12 @@
 type t =
-{ id : int
-; name : string
-; origin : string option }
+  { id : int
+  ; name : string
+  ; origin : string option }
 
 let of_caqti_tuple tuple =
   let (id, (name, origin)) = tuple in
   {id; name; origin}
 
 let option_to_string = function
-| None -> "<None>"
-| Some s -> s
+  | None -> "<None>"
+  | Some s -> s
